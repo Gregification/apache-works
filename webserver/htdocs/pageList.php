b7871744aa030a6131ed7f3acf4c57ce6952ebf0
gregclacker <yongboone1@gmail.com>
2023-10-01 09:54:32 -0500

    clean

--- webserver/htdocs/pageList.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = array("a" => "Apple", "b" => "Ball", "c" => "Cat", "asdpaaeiocnpaec"=>"weeeeeeeeeeee");
-
-header("Content-Type: application/json");
-echo json_encode($data);
-exit();
-?>
--- webserver/private_request/Patterns.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace private_request;
-
-    class Patterns {
-        const EMAIL = "/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix";
-        const INT = "/^\d+$/";
-        const USERNAME = "/^\w+$/";
-    }
-?>
